4ae46d39bb0a36d63ebdf0252a14d63f7409fc8d
jahnavi240605 <jahnavikurra24@gmail.com>
2025-06-10 21:25:38 +0530

    Add files via upload

---
+++ String2.java
@@ -0,0 +1,10 @@
+public class String2
+{
+	public static void main(String[] args) {
+	    String a="madhavi";
+	    for( int i=0;i<a.length();i++)
+	    {
+		System.out.println(a.charAt(i));
+	}
+	}
+}
